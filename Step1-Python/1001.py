7510f2bc2a332b9f98b8a3a1950cf19311d7ffc7
SuJin Park <psjin230@gmail.com>
2022-02-21 00:06:26 +0900

    step1-2

---
+++ Step1-Python/1001.py
@@ -0,0 +1,7 @@
+#한번에 입력값 2개 받아야함.
+a,b=input().split()
+print(int(a)-int(b))
+
+#입력받으면서 int형으로 바꾸기
+c,d=map(int,input().split())
+print(c-d)
---
+++ Step1-Python/1008.py
@@ -0,0 +1,7 @@
+#한번에 입력값 2개 받아야함.
+a,b=input().split()
+print(float(a)/float(b))
+
+#입력받으면서 int형으로 바꾸기
+c,d=map(float,input().split())
+print(c/d)
