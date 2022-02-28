0c41ef26d085dcf212a0b9d66455916f28501995
SuJin Park <psjin230@gmail.com>
2022-02-28 21:21:18 +0900

    step4-2

--- Step4-Python/1110.py
+++ Step4-Python/1110.py
@@ -1,18 +1,20 @@
-num=int(input())
+num=int(input()) #초기값
+cmp_num=num #비교값
 count=0
 
 while True:
-    first=num//10 #몫=십의자리  
-    last=num%10 #나머지=일의자리  
+    ten=cmp_num//10 #몫=십의자리  
+    one=cmp_num%10 #나머지=일의자리  
 
-    result=first+last 
-    count+=1
+    #두 수를 더해 result값 만들기
+    result=ten+one
+    
+    #새로운 비교값만들기
+    cmp_num=one*10+result%10
 
-    new_result=num%10+result%10
- 
-    if num==new_result:
+    count+=1 #사이클 계산
+    #원래 값과 새로운값 비교
+    if num==cmp_num:
         break
 
 print(count)
-
-##아직 다 못품..
