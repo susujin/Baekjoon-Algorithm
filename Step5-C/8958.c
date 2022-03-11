f1aba8d9692deb36c47541ff47da7455d304494d
SuJin Park <psjin230@gmail.com>
2022-03-11 22:39:51 +0900

    step5-10

---
+++ Step5-C/8958.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main_abc()
+{
+	int num;
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++)
+	{
+		char ox[81];
+		scanf("%s", ox);
+
+		int sum = 0;
+		int add_num = 0;
+
+		for (int j = 0; j < strlen(ox); j++)
+		{
+			if (ox[j] == 'O')
+			{
+				add_num += 1;
+				sum += add_num;
+			}
+			else
+				add_num = 0;
+		}
+		printf("%d\n", sum);
+	}
+	return 0;
+}
